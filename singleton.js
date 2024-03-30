e6c3873de742e8cd576e9544ec51bd07b98dffa8
vanCBarE <ibarragan@unal.edu.co>
2024-03-29 19:23:39 -0600

    module singleton

---
+++ singleton.js
@@ -0,0 +1,18 @@
+/*
+* how to implement singleton?
+*
+* 1. Make the constructor private
+* 2. Create a static method who calls the private
+* constructor and save the instance in a static variable
+*/
+class Singleton {
+    constructor() {
+
+    }
+}
+
+function appSingleton() {
+
+}
+
+appSingleton();
